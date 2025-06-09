d9b2a49ce24daef29d933cc59309c5a0cf92d8bc
Kevin Alavik <kevin@alavik.se>
2025-06-09 17:03:05 +0200

    fix/nvim: Fixed nvim-cmp

---
+++ nvim/lua/plugins/cmp.lua
@@ -0,0 +1,57 @@
+local cmp = require("cmp")
+
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+cmp.setup({
+  mapping = {
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-d>"] = cmp.mapping.scroll_docs(4),
+
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  },
+
+  sources = {
+    { name = "nvim_lsp", priority = 1000 },
+    { name = "buffer", priority = 500 },
+    { name = "path", priority = 250 },
+  },
+
+  formatting = {
+    format = function(entry, item)
+      local source_names = {
+        nvim_lsp = "[LSP]",
+        buffer = "[Buffer]",
+        path = "[Path]",
+      }
+      item.menu = source_names[entry.source.name] or ""
+      return item
+    end,
+  },
+
+  experimental = {
+    ghost_text = true,
+  },
+})
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,5 +1,4 @@
 local lspconfig = require("lspconfig")
-local cmp = require("cmp")
 local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
 -- Capabilities
@@ -18,49 +17,28 @@ lspconfig.asm_lsp.setup {
 
 lspconfig.lua_ls.setup {
     capabilities = capabilities,
+    on_init = function(client)
+        client.server_capabilities.didChangeWatchedFiles = false
+    end,
     settings = {
         Lua = {
             runtime = {
-                version = 'LuaJIT',
-                path = vim.split(package.path, ';'),
+                version = "LuaJIT",
+                path = vim.split(package.path, ";"),
             },
             diagnostics = {
-                globals = { 'vim' },
+                globals = { "vim" },
             },
             workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
+                library = {
+                    vim.api.nvim_get_runtime_file("", true)
+                },
                 checkThirdParty = false,
+                preloadFileSize = 1000,
             },
-            telemetry = { enable = false },
+            telemetry = {
+                enable = false,
+            },
         },
     },
 }
-
--- Completion
-cmp.setup {
-    mapping = {
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-k>"] = cmp.mapping.select_prev_item(),
-        ["<C-j>"] = cmp.mapping.select_next_item(),
-        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-d>"] = cmp.mapping.scroll_docs(4),
-    },
-    sources = {
-        { name = "nvim_lsp", priority = 1000 },
-        { name = "buffer", priority = 500 },
-        { name = "path", priority = 250 },
-    },
-    formatting = {
-        format = function(entry, item)
-            item.menu = ({
-                nvim_lsp = "[LSP]",
-                buffer = "[Buffer]",
-                path = "[Path]",
-            })[entry.source.name]
-            return item
-        end,
-    },
-    experimental = { ghost_text = true },
-}
-
